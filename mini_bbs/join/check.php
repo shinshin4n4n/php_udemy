56d265453f0484ea91a5ea7ac9c3fb9ed984a1b1
Shin Iguchi <s.iguchi777@gmail.com>
2020-12-19 14:35:58 +0900

    エラー修正

--- mini_bbs/join/check.php
+++ mini_bbs/join/check.php
@@ -11,7 +11,7 @@ function h($s){
 	if(!isset($_SESSION['join'])){
 		header('Location: index.php');
 		exit();
-	}ss
+	}
 
 ?>
 
--- mini_bbs/join/index.php
+++ mini_bbs/join/index.php
@@ -23,9 +23,10 @@
 		}
 
 		if(empty($error)){
-			$image = date('YmdHis').$_FILES['image'].['name'];
-			move_uploaded_file($_FILES['image'].['tmp_name'],
-			'../member_picture/'.$image);
+		
+		$image = date('YmdHis') . $_FILES['image'].['name'];
+		move_uploaded_file($_FILES['image'].['tmp_name'],
+		'../member_picture/' . $image);
 
 		$_SESSION['join'] = $_POST;
 		header('Location: check.php');
